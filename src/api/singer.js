0140164e4fda1707347af04b3b72cc2acf3f3b24
wanghaoran <wanghaoran@corp.netease.com>
2018-06-05 23:27:37 +0800

    测试代码提交

--- src/api/singer.js
+++ src/api/singer.js
@@ -1,1 +1,8 @@
+import axios from 'axios'
+import {HOST} from 'common/js/config'
 
+//  获得热门歌手信息
+export function getSingers () {
+  const url = HOST + '/toplist/artist'
+  return axios.get(url)
+}
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -4,6 +4,28 @@
   </div>
 </template>
 <script type="text/ecmascript-6">
+import {getSingers} from 'api/singer'
+import {ERR_OK} from 'common/js/config'
+export default {
+  data () {
+    return {
+      singers: []
+    }
+  },
+  created () {
+    this._getSingers()
+  },
+  methods: {
+    _getSingers () {
+      getSingers().then((res) => {
+        if (res.status === ERR_OK) {
+          let s = res.data.list.artists
+          console.log(res,s)
+        }
+      })
+    }
+  }
+}
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
 </style>
